bbffe31efdda122d89cd0c3f397b5b131ce82f31
joseph <joseph.beckett@academytrainee.com>
2020-04-22 16:10:26 +0100

    jsb calculato done-o

--- src/main/java/testingScuffLand/scuffedMain.java
+++ src/main/java/testingScuffLand/scuffedMain.java
@@ -1,6 +1,9 @@
 package testingScuffLand;
 
+// import  DumbCalculator.CalculateDumbStuff;
+
 public class scuffedMain {
+	//static CalculateDumbStuff cds;
 	public static void main(String[] args) {
 		for (int i = 0; i <= 100; i++) {
 			String[] sufixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
@@ -13,6 +16,9 @@ public class scuffedMain {
 				System.out.println(i + sufixes[i % 10]);
 			}
 		}
+//		int[] intAr = {2,5,7,9,2,4,89,6,1,1,2,4};
+//		 cds = new CalculateDumbStuff(intAr);
+//		cds.dewIt();
 
 	}
 }
